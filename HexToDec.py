ece5931b648f682dab55c2e92f4d8c8e26eaa676
Catalina Escalona <68168149+catalinaescalona@users.noreply.github.com>
2021-03-01 21:28:38 +0100

    Update HexToDec.py

--- HexToDec.py
+++ HexToDec.py
@@ -1,5 +1,14 @@
+hex_dict = {"A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15}
+
 def hex_to_decimal(lst):
     n = 0
+    s = 1
     for num in lst:
-        n = 16*n + num
+        power = len(lst) - s
+        if num in hex_dict:
+            k = hex_dict.get(num)
+            n += (16**power) * k
+        else:
+            n += (16**power) * num
+        s += 1
     return n
